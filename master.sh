40423c932ddb5635881f668ae0e4446129761f6e
jcqueiroz <jeancq-test1@outlook.com>
2022-11-06 19:18:26 -0300

    increase for 15 in nginx-replicas

--- master.sh
+++ master.sh
@@ -4,4 +4,4 @@ sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
 sudo apt install curl -y
 sudo curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
 sudo docker stack deploy --compose-file=portainer-agent-stack.yml portainer
-sudo docker service create --name my_nginx --replicas 8 --publish published=8080,target=80 nginx
+sudo docker service create --name my_nginx --replicas 15 --publish published=8080,target=80 nginx
